718b0d01f8da6a2abcaf97e8a44cb7c8d2957ff8
nargiza1903 <121645300+nargiza1903@users.noreply.github.com>
2023-07-29 14:31:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+const dns = require("dns");
+const { nanoid } = require("nanoid");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+const urlDatabase = {};
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl/new", function (req, res) {
+  const { url } = req.body;
+
+  // Check if the URL is valid
+  const urlObj = new URL(url);
+  const hostname = urlObj.hostname;
+
+  dns.lookup(hostname, async (err) => {
+    if (err) {
+      res.json({ error: "Invalid URL" });
+    } else {
+      const id = nanoid(6); // Generate a random alphanumeric ID with length 6
+      urlDatabase[id] = url;
+      res.json({ original_url: url, short_url: id });
+    }
+  });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:id", function (req, res) {
+  const id = req.params.id;
+  const url = urlDatabase[id];
+
+  if (url) {
+    res.redirect(url);
+  } else {
+    res.json({ error: "Short URL not found" });
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
