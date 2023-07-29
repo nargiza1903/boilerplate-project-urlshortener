a35807344206e4e25401e34cc5aefd845723cbd5
nargiza1903 <121645300+nargiza1903@users.noreply.github.com>
2023-07-29 15:36:58 +0200

    Update final index.js

--- index.js
+++ index.js
@@ -1,65 +1,76 @@
+// Load environment variables from .env file if present
 require('dotenv').config();
+
+// Import required modules
 const express = require('express');
 const cors = require('cors');
-const dns = require('dns');
-const bodyParser = require('body-parser');
-
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const shortid = require('shortid');
+
+// Use body-parser middleware to parse incoming request bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Object to store URL mappings
+const urls = {};
+
+// Set the port number for the server
 const port = process.env.PORT || 3000;
 
+// Enable Cross-Origin Resource Sharing (CORS) for all routes
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
 
+// Serve static files from the 'public' directory
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+// Route to serve the index.html file
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-const urlDatabase = {}; // A simple in-memory database to store the URLs
-let counter = 0; // A counter to generate unique short URLs
-
-// Your first API endpoint
+// API endpoint to greet the user
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// API endpoint to handle URL shortening
 app.post('/api/shorturl', function(req, res) {
   const originalUrl = req.body.url;
 
-  // Check if the URL is valid using dns.lookup
-  dns.lookup(originalUrl, (err, address, family) => {
+  // Parse the provided URL
+  const parsedUrl = new URL(originalUrl);
+  
+  // Validate the URL using DNS lookup
+  dns.lookup(parsedUrl.hostname, function(err, address, family) {
     if (err) {
-      return res.json({ error: 'invalid url' });
+      // If the URL is invalid, send an error response
+      res.json({ error: 'invalid url' });
+    } else {
+      // Generate a short URL using the 'shortid' library and store the mapping
+      const shortUrl = shortid.generate();
+      urls[shortUrl] = originalUrl;
+      // Respond with the original and short URLs
+      res.json({ original_url: originalUrl, short_url: shortUrl });
     }
-
-    // Generate a unique short URL
-    const shortUrl = generateShortUrl();
-
-    // Store the URL in the database
-    urlDatabase[shortUrl] = originalUrl;
-
-    // Respond with the short URL and original URL
-    res.json({ original_url: originalUrl, short_url: shortUrl });
   });
 });
 
-app.get('/api/shorturl/:shortUrl', function(req, res) {
-  const shortUrl = req.params.shortUrl;
-  const originalUrl = urlDatabase[shortUrl];
-
+// API endpoint to handle redirection based on short URLs
+app.get('/api/shorturl/:id', function(req, res) {
+  const shortUrl = req.params.id;
+  // Look up the original URL from the 'urls' object using the short URL
+  const originalUrl = urls[shortUrl];
   if (originalUrl) {
-    // Redirect to the original URL
+    // If the short URL exists, redirect the user to the original URL
     res.redirect(originalUrl);
   } else {
-    res.json({ error: 'short URL not found' });
+    // If the short URL does not exist, send an error response
+    res.json({ error: 'invalid url' });
   }
 });
 
+// Start the server and listen on the specified port
 app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
-
-function generateShortUrl() {
-  // Generate a unique short URL using a simple counter and increment it for each new URL
-  return (++counter).toString();
-}
